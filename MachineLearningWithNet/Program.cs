ea738b929bb76bd788b0185203de1b253b0f4518
AmanSonii <a.soni@eicare.org>
2021-03-26 14:13:22 +0530

    add initial code

---
+++ MachineLearningWithNet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MachineLearningWithNetML.Model;
+namespace MachineLearningWithNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput();
+            input.SentimentText = "This is Ok";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+            Console.ReadKey();
+        }
+    }
+}
